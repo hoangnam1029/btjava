3dd3313f694d9372bdefabc4b15167451487c33e
hoangnam1029 <hoangman1029@gmai.com>
2023-11-07 19:23:28 +0700

    btdk2

---
+++ app/src/main/java/com/java/btjava1/btdieukien/btdk2.java
@@ -0,0 +1,14 @@
+package com.java.btjava1.btdieukien;
+
+public class btdk2 {
+    public static void main(String[] args) {
+        int soNguyen = 3;
+        if (soNguyen == 1) {
+            System.out.println("Một");
+        } else if (soNguyen == 2) {
+            System.out.println("Hai");
+        } else {
+            System.out.println("Ba");
+        }
+    }
+}
