b87d89f7700b9cb46251038775d5e3e4b61c6057
hoangnam1029 <hoangman1029@gmai.com>
2023-11-07 19:46:45 +0700

    btvl1

--- app/src/main/java/com/java/btjava1/btvonglap/btvl1.java
+++ app/src/main/java/com/java/btjava1/btvonglap/btvl1.java
@@ -3,18 +3,18 @@ package com.java.btjava1.btvonglap;
 public class btvl1 {
     public static void main(String[] args){
         int n = 10;
-        int s = 0;
+        int sum = 0;
         if(n<0){
             System.out.println("Nhap lai");
             return;
         }else{
             for(int i=0;i<=n;i++){
                 if(i%2==0){
-                    s+=i;
+                    sum+=i;
                     i++;
                 }
             }
         }
-        System.out.println("Tong cac so chan: " + s);
+        System.out.println("Tong cac so chan: " + sum);
     }
 }
